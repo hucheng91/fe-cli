ee8426e37e36f85d273e5071054c09a70ace1634
nigel <1149828479@qq.com>
2020-12-03 15:38:08 +0800

    feat:delete unused template

--- myTest/config/dev.env.js
+++
@@ -1,8 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  //API_ROOT: '"http://172.25.16.167:1024/6tnitj!25"'
-  API_ROOT: '""'
-})
--- myTest/src/vuex/mutation-types.js
+++
@@ -1,3 +0,0 @@
-export const COLLAPSE_SIDEBAR_MENU = 'collapse_sidebar_menu';
-export const SET_ACCOUNT_INFO = 'set_account_info';
-export const SET_LOCALS = 'set_locals';
